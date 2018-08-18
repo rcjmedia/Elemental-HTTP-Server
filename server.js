2be3819e1db884b27685b1633d7a6f13b65fd1fd
Romeo <romeocorpuz808@gmail.com>
2018-08-18 11:52:43 -1000

    got 404.html working

--- server.js
+++ server.js
@@ -2,21 +2,10 @@ var http = require('http');
 var fs = require('fs');
 var PORT = 9090;
 var PUBLIC = './public/';
+var notfound = './public/404.html';
 var server = http.createServer(requestFunction)
 
-server.listen(PORT,function(){
-  console.log('http server listening on port:'+PORT)
-})
-
-function requestFunction(request, response){
-
-  switch(request.method){
-    case 'GET':
-      functionGet(request,response);
-      break;
-  }
-}
-
+///GET function
 function functionGet(request,response){
   var uri = request.url;
     if(uri === '/'){
@@ -32,10 +21,25 @@ function functionGet(request,response){
           response.write(data);
           response.end();
         })
-      } else{
-        response.statusCode = 404;
-        response.write('<h1>File does not exist!</h1>');
-        response.end();
+      } else { //read the 404.html file if page not found
+        fs.readFile(notfound, 'utf8', (err, data) => {
+          response.writeHead(404);
+          response.write(data);
+          response.end();
+        })
       }
     });
 }
+
+///Helper func for GET
+function requestFunction(request, response){
+  switch(request.method){
+    case 'GET':
+      functionGet(request,response);
+      break;
+  }
+}
+
+server.listen(PORT,function(){
+  console.log('http server listening on port:'+PORT)
+})
