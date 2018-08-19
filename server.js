89684070024646a281bdabc6b21f0730e7844fa7
Romeo <romeocorpuz808@gmail.com>
2018-08-18 19:17:02 -1000

    made changes

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 var http = require('http');
 var fs = require('fs');
-var PORT = 9090;
+var querystring = require('querystring');
+var PORT = 8080;
 var PUBLIC = './public/';
 var notfound = './public/404.html';
 var server = http.createServer(requestFunction)
+var postSuccessful = '{"success":"true"}';
+
 
 ///GET function
 function functionGet(request,response){
@@ -14,7 +17,7 @@ function functionGet(request,response){
 
     fs.exists(PUBLIC + uri, function(exists){
       if(exists){
-        fs.readFile(PUBLIC + uri,function(err,data){
+        fs.readFile(PUBLIC + uri,function(err,data) {
           if(err){
             throw err;
           }
@@ -31,15 +34,54 @@ function functionGet(request,response){
     });
 }
 
-///Helper func for GET
+
+/////POST function
+function functionPost(request,response){
+  var postBody = '';
+  request.on('data', function(chunk){
+  postBody += chunk.toString();
+  // response.end();
+  })
+  request.on('end', function(){
+    var postObject = querystring.parse(postBody);
+    var pageContent = generateElementHtmlPage(postObject.elementName,postObject.elementSymbol,postObject.elementAtomicNumber,postObject.elementDescription);
+    var fileName = PUBLIC+postObject.elementName.toLowerCase() +'.html';
+    //check if file exists
+      fs.exists(fileName,function(fileAlreadyExists){
+        if (fileAlreadyExists){
+        //if file exists respond unsucessful
+        response.write(postUnsuccessful);
+        response.end();
+        } else{
+        // otherwise create a new file, in public and respond success
+          fs.writeFile(fileName,pageContent,function(err){
+            if(err){
+            response.write(err);
+            response.end();
+            throw err;
+            } else{
+            response.write(postSuccessful);
+            response.end();
+            updateIndexPage(fileName,postObject.elementName)
+            }
+          })
+        }
+      })
+  })
+  }
+
+////////Helper func for GET, POST////////
 function requestFunction(request, response){
   switch(request.method){
     case 'GET':
       functionGet(request,response);
       break;
+    case 'POST':
+      functionPost(request,response);
+      break;  
   }
 }
 
 server.listen(PORT,function(){
-  console.log('http server listening on port:'+PORT)
+  console.log('HTTP server listening on port: '+PORT)
 })
